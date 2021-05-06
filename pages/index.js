eb291a57dc5bef898daa5a2966c98a62e5ff5cbd
simon-ugor <simiugor@gmail.com>
2021-05-06 15:32:21 +0200

    Color change

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@
 const index = () => {
     return (
         <div className="App">
-            <h3 className="coming-soon">Coming soon</h3>
+            <h3 className="coming-soon">Coming soon.</h3>
         </div>
     )
 }
