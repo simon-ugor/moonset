642a1e2fc2f8aaffc59dc3810e231ad07894986b
simon-ugor <simiugor@gmail.com>
2021-05-14 01:24:25 +0200

    Rotating pizza

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
 
+import Image from "next/image"
+
 const index = () => {
     return (
         <div className="App">
             <div className="coming-soon-div">
                 <h3 className="coming-soon">Coming soon.</h3>
+                <div className="image-div">
+                    <Image
+                        src="/pizza-cutout.png"
+                        alt="sun"
+                        width={60}
+                        height={49}
+                        className="image"
+                    />
+                </div>
             </div>
         </div>
     )
