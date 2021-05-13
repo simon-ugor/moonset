c5c6c92a8c39f52e33531f63c02496a84f90da5c
simon-ugor <simiugor@gmail.com>
2021-05-14 00:54:11 +0200

    delete it all

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ const index = () => {
     return (
         <div className="App">
             <h3 className="coming-soon">Coming soon.</h3>
-            <div className="background"></div>
         </div>
     )
 }
