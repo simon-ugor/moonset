ea269cb8c537eeee2ea99501c6b30ffa9559ca16
simon-ugor <simiugor@gmail.com>
2021-05-14 01:01:03 +0200

    getting mad

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,9 @@
 const index = () => {
     return (
         <div className="App">
-            <h3 className="coming-soon">Coming soon.</h3>
+            <div className="full-screen-div">
+                <h3 className="coming-soon">Coming soon.</h3>
+            </div>
         </div>
     )
 }
